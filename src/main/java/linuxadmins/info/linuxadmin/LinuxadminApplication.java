68a800e6e4ee304deb243eb072d81efb6183a14c
student <shebinbabu05@gmail.com>
2021-05-29 15:02:58 +0000

    Added a springboot ap

---
+++ src/main/java/linuxadmins/info/linuxadmin/LinuxadminApplication.java
@@ -0,0 +1,13 @@
+package linuxadmins.info.linuxadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/linuxadmins/info/linuxadmin/LinuxadminApplicationTests.java
@@ -0,0 +1,13 @@
+package linuxadmins.info.linuxadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
